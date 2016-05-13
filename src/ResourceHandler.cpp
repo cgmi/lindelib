18f3502d2527daf2d06d8414d3cccafe26218908
Thomas Lindemeier <thomas.lindemeier@uni-konstanz.de>
2016-05-13 13:14:11 +0200

    resource.path in same level also possible

--- src/ResourceHandler.cpp
+++ src/ResourceHandler.cpp
@@ -7,23 +7,28 @@ static std::string localPathToResource;
 
 std::string ResourceHandler::getResource(const std::string &s)
 {
-	{
-		if (localPathToResource.empty())
-		{
-			// local path not yet loaded
-			std::ifstream stream("../resources.path");
-			if (stream.is_open())
-			{
-				std::getline(stream, localPathToResource);
-			}
-			else
-			{
-				// no 'resources.path' file
-				std::cout << "Could not find your 'resources.path' file!\n";
-				return std::string();
-			}
+    if (localPathToResource.empty())
+    {
+        // local path not yet loaded
+        std::ifstream stream("../resources.path");
+        std::ifstream stream2("resources.path");
+        if (stream.is_open())
+        {
+            std::getline(stream, localPathToResource);
             stream.close();
-		}
-		return localPathToResource + s;
-	}
+        } else if (stream2.is_open())
+        {
+            std::getline(stream2, localPathToResource);
+            stream2.close();
+        }
+        else
+        {
+            // no 'resources.path' file
+            std::cerr << "Could not find your 'resources.path' file!\n";
+            std::cin.get();
+            return std::string();
+        }
+
+    }
+    return localPathToResource + s;
 }
