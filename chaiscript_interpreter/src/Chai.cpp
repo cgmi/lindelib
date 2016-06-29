9095691ca616f93271b82de022edcd924f6f9601
Thomas Lindemeier <thomas.lindemeier@uni-konstanz.de>
2016-06-29 11:12:43 +0200

    vect type operators

--- chaiscript_interpreter/src/Chai.cpp
+++ chaiscript_interpreter/src/Chai.cpp
@@ -32,7 +32,7 @@ cv::Mat_<T> gaussian_blur(const cv::Mat_<T>& source, float sigma)
 
 template <class T>
 cv::Mat_<T> ComputeGaussDerivativeEnergy(const cv::Mat_<T> & source,
-                             const float sigma, const int nrAngles)
+                                         const float sigma, const int nrAngles)
 {
     cv::Mat_<T> out;
     linde::ComputeGaussDerivativeEnergy(source, out, sigma, nrAngles);
@@ -83,45 +83,104 @@ void addVectorTypes(chaiscript::ChaiScript &chai)
 
     // vector types
     chaiscript::utility::add_class<glm::vec3>(*m,
-                                   "vec3",
+                                              "vec3",
     {
-        chaiscript::constructor<glm::vec3()>(),
-        chaiscript::constructor<glm::vec3(float, float, float)>()
-    },
+                                                  chaiscript::constructor<glm::vec3()>(),
+                                                  chaiscript::constructor<glm::vec3(float, float, float)>()
+                                              },
     {
-        { chaiscript::fun(&glm::vec3::x), "x" },
-        { chaiscript::fun(&glm::vec3::y), "y" },
-        { chaiscript::fun(&glm::vec3::z), "z" }
-    });
+                                                  { chaiscript::fun(&glm::vec3::x), "x" },
+                                                  { chaiscript::fun(&glm::vec3::y), "y" },
+                                                  { chaiscript::fun(&glm::vec3::z), "z" },
+
+                                                  { chaiscript::fun<glm::vec3&, glm::vec3, glm::vec3 const &>(&glm::vec3::operator=), "=" },
+                                                  { chaiscript::fun<glm::vec3&, glm::vec3, glm::vec3 const &>(&glm::vec3::operator+=), "+=" },
+                                                  { chaiscript::fun<glm::vec3&, glm::vec3, glm::vec3 const &>(&glm::vec3::operator-=), "-=" },
+                                                  { chaiscript::fun<glm::vec3&, glm::vec3, glm::vec3 const &>(&glm::vec3::operator/=), "/=" },
+                                                  { chaiscript::fun<glm::vec3&, glm::vec3, glm::vec3 const &>(&glm::vec3::operator*=), "*=" }
+                                              });
 
 
     chaiscript::utility::add_class<glm::vec2>(*m,
-                                   "vec2",
+                                              "vec2",
     {
-        chaiscript::constructor<glm::vec2()>(),
-        chaiscript::constructor<glm::vec2(float, float)>()
-    },
+                                                  chaiscript::constructor<glm::vec2()>(),
+                                                  chaiscript::constructor<glm::vec2(float, float)>()
+                                              },
     {
-        { chaiscript::fun(&glm::vec2::x), "x" },
-        { chaiscript::fun(&glm::vec2::y), "y" }
-    });
+                                                  { chaiscript::fun(&glm::vec2::x), "x" },
+                                                  { chaiscript::fun(&glm::vec2::y), "y" },
+
+                                                  { chaiscript::fun<glm::vec2&, glm::vec2, glm::vec2 const &>(&glm::vec2::operator=), "=" },
+                                                  { chaiscript::fun<glm::vec2&, glm::vec2, glm::vec2 const &>(&glm::vec2::operator+=), "+=" },
+                                                  { chaiscript::fun<glm::vec2&, glm::vec2, glm::vec2 const &>(&glm::vec2::operator-=), "-=" },
+                                                  { chaiscript::fun<glm::vec2&, glm::vec2, glm::vec2 const &>(&glm::vec2::operator/=), "/=" },
+                                                  { chaiscript::fun<glm::vec2&, glm::vec2, glm::vec2 const &>(&glm::vec2::operator*=), "*=" }
+                                              });
 
     chaiscript::utility::add_class<glm::ivec2>(*m,
-                                   "ivec2",
+                                               "ivec2",
     {
-        chaiscript::constructor<glm::ivec2()>(),
-        chaiscript::constructor<glm::ivec2(int, int)>()
-    },
+                                                   chaiscript::constructor<glm::ivec2()>(),
+                                                   chaiscript::constructor<glm::ivec2(int, int)>()
+                                               },
     {
-        { chaiscript::fun(&glm::ivec2::x), "x" },
-        { chaiscript::fun(&glm::ivec2::y), "y" }
-    });
+                                                   { chaiscript::fun(&glm::ivec2::x), "x" },
+                                                   { chaiscript::fun(&glm::ivec2::y), "y" },
+
+                                                   { chaiscript::fun<glm::ivec2&, glm::ivec2, glm::ivec2 const &>(&glm::ivec2::operator=), "=" },
+                                                   { chaiscript::fun<glm::ivec2&, glm::ivec2, glm::ivec2 const &>(&glm::ivec2::operator+=), "+=" },
+                                                   { chaiscript::fun<glm::ivec2&, glm::ivec2, glm::ivec2 const &>(&glm::ivec2::operator-=), "-=" },
+                                                   { chaiscript::fun<glm::ivec2&, glm::ivec2, glm::ivec2 const &>(&glm::ivec2::operator/=), "/=" },
+                                                   { chaiscript::fun<glm::ivec2&, glm::ivec2, glm::ivec2 const &>(&glm::ivec2::operator*=), "*=" }
+                                               });
+
+    m->add(chaiscript::fun<glm::vec3, glm::vec3 const &, glm::vec3 const &>(&glm::operator+), "+");
+    m->add(chaiscript::fun<glm::vec3, glm::vec3 const &, glm::vec3 const &>(&glm::operator-), "-");
+    m->add(chaiscript::fun<glm::vec3, glm::vec3 const &, glm::vec3 const &>(&glm::operator/), "/");
+    m->add(chaiscript::fun<glm::vec3, glm::vec3 const &, glm::vec3 const &>(&glm::operator*), "*");
+    m->add(chaiscript::fun<glm::vec3, glm::vec3 const &, float const &>(&glm::operator+), "+");
+    m->add(chaiscript::fun<glm::vec3, glm::vec3 const &, float const &>(&glm::operator-), "-");
+    m->add(chaiscript::fun<glm::vec3, glm::vec3 const &, float const &>(&glm::operator/), "/");
+    m->add(chaiscript::fun<glm::vec3, glm::vec3 const &, float const &>(&glm::operator*), "*");
+    m->add(chaiscript::fun<glm::vec3, float const &, glm::vec3 const &>(&glm::operator+), "+");
+    m->add(chaiscript::fun<glm::vec3, float const &, glm::vec3 const &>(&glm::operator-), "-");
+    m->add(chaiscript::fun<glm::vec3, float const &, glm::vec3 const &>(&glm::operator/), "/");
+    m->add(chaiscript::fun<glm::vec3, float const &, glm::vec3 const &>(&glm::operator*), "*");
+
+    m->add(chaiscript::fun<glm::vec2, glm::vec2 const &, glm::vec2 const &>(&glm::operator+), "+");
+    m->add(chaiscript::fun<glm::vec2, glm::vec2 const &, glm::vec2 const &>(&glm::operator-), "-");
+    m->add(chaiscript::fun<glm::vec2, glm::vec2 const &, glm::vec2 const &>(&glm::operator/), "/");
+    m->add(chaiscript::fun<glm::vec2, glm::vec2 const &, glm::vec2 const &>(&glm::operator*), "*");
+    m->add(chaiscript::fun<glm::vec2, glm::vec2 const &, float const &>(&glm::operator+), "+");
+    m->add(chaiscript::fun<glm::vec2, glm::vec2 const &, float const &>(&glm::operator-), "-");
+    m->add(chaiscript::fun<glm::vec2, glm::vec2 const &, float const &>(&glm::operator/), "/");
+    m->add(chaiscript::fun<glm::vec2, glm::vec2 const &, float const &>(&glm::operator*), "*");
+    m->add(chaiscript::fun<glm::vec2, float const &, glm::vec2 const &>(&glm::operator+), "+");
+    m->add(chaiscript::fun<glm::vec2, float const &, glm::vec2 const &>(&glm::operator-), "-");
+    m->add(chaiscript::fun<glm::vec2, float const &, glm::vec2 const &>(&glm::operator/), "/");
+    m->add(chaiscript::fun<glm::vec2, float const &, glm::vec2 const &>(&glm::operator*), "*");
+
+    m->add(chaiscript::fun<glm::ivec2, glm::ivec2 const &, glm::ivec2 const &>(&glm::operator+), "+");
+    m->add(chaiscript::fun<glm::ivec2, glm::ivec2 const &, glm::ivec2 const &>(&glm::operator-), "-");
+    m->add(chaiscript::fun<glm::ivec2, glm::ivec2 const &, glm::ivec2 const &>(&glm::operator/), "/");
+    m->add(chaiscript::fun<glm::ivec2, glm::ivec2 const &, glm::ivec2 const &>(&glm::operator*), "*");
+    m->add(chaiscript::fun<glm::ivec2, glm::ivec2 const &, int const &>(&glm::operator+), "+");
+    m->add(chaiscript::fun<glm::ivec2, glm::ivec2 const &, int const &>(&glm::operator-), "-");
+    m->add(chaiscript::fun<glm::ivec2, glm::ivec2 const &, int const &>(&glm::operator/), "/");
+    m->add(chaiscript::fun<glm::ivec2, glm::ivec2 const &, int const &>(&glm::operator*), "*");
+    m->add(chaiscript::fun<glm::ivec2, int const &, glm::ivec2 const &>(&glm::operator+), "+");
+    m->add(chaiscript::fun<glm::ivec2, int const &, glm::ivec2 const &>(&glm::operator-), "-");
+    m->add(chaiscript::fun<glm::ivec2, int const &, glm::ivec2 const &>(&glm::operator/), "/");
+    m->add(chaiscript::fun<glm::ivec2, int const &, glm::ivec2 const &>(&glm::operator*), "*");
 
     chai.add(m);
 }
 
 void addImageTypes(chaiscript::ChaiScript &chai)
 {
+    addVectorTypes(chai);
+
     chaiscript::ModulePtr m(new chaiscript::Module());
 
     m->add(chaiscript::base_class<cv::Mat, cv::Mat_<glm::vec3> >());
@@ -142,39 +201,43 @@ void addImageTypes(chaiscript::ChaiScript &chai)
     m->add(chaiscript::fun(&createMat3_vec_value), "Mat3");
     m->add(chaiscript::fun(&createMat1_vec_value), "Mat1");
 
-    m->add(chaiscript::fun<float&, cv::Mat_<float>, int, int>(&cv::Mat_<float>::at<float>), "at");
-    m->add(chaiscript::fun<glm::vec3&, cv::Mat_<glm::vec3>, int, int>(&cv::Mat_<glm::vec3>::at<glm::vec3>), "at");
-    m->add(chaiscript::fun<float&, cv::Mat_<float>, int>(&cv::Mat_<float>::at<float>), "at");
-    m->add(chaiscript::fun<glm::vec3&, cv::Mat_<glm::vec3>, int>(&cv::Mat_<glm::vec3>::at<glm::vec3>), "at");
+    m->add(chaiscript::fun<float&, cv::Mat_<float>, int, int>(&cv::Mat_<float>::operator()), "at");
+    m->add(chaiscript::fun<glm::vec3&, cv::Mat_<glm::vec3>, int, int>(&cv::Mat_<glm::vec3>::operator()), "at");
+    m->add(chaiscript::fun<float&, cv::Mat_<float>, int>(&cv::Mat_<float>::operator()), "at");
+    m->add(chaiscript::fun<glm::vec3&, cv::Mat_<glm::vec3>, int>(&cv::Mat_<glm::vec3>::operator()), "at");
+
+    m->add(chaiscript::fun<const float&, cv::Mat_<float>, int, int>(&cv::Mat_<float>::operator()), "at");
+    m->add(chaiscript::fun<const glm::vec3&, cv::Mat_<glm::vec3>, int, int>(&cv::Mat_<glm::vec3>::operator()), "at");
+    m->add(chaiscript::fun<const float&, cv::Mat_<float>, int>(&cv::Mat_<float>::operator()), "at");
+    m->add(chaiscript::fun<const glm::vec3&, cv::Mat_<glm::vec3>, int>(&cv::Mat_<glm::vec3>::operator()), "at");
 
     chai.add(m);
 }
 
 void addImageIO(chaiscript::ChaiScript &chai)
 {
-     chaiscript::ModulePtr m(new chaiscript::Module());
+    chaiscript::ModulePtr m(new chaiscript::Module());
 
-     // image load and save
-     m->add(chaiscript::fun<cv::Mat_<glm::vec3>, const std::string&>(&linde::imLoad), "imload");
-     m->add(chaiscript::fun<cv::Mat_<float>, const std::string&>(&linde::imLoadSingleChannel), "imload_single_channel");
+    // image load and save
+    m->add(chaiscript::fun<cv::Mat_<glm::vec3>, const std::string&>(&linde::imLoad), "imload");
+    m->add(chaiscript::fun<cv::Mat_<float>, const std::string&>(&linde::imLoadSingleChannel), "imload_single_channel");
 
-     m->add(chaiscript::fun<bool, const std::string&, const cv::Mat_<glm::vec3>&>(&linde::imSave), "imsave");
-     m->add(chaiscript::fun<bool, const std::string&, const cv::Mat_<float>&>(&linde::imSave), "imsave");
+    m->add(chaiscript::fun<bool, const std::string&, const cv::Mat_<glm::vec3>&>(&linde::imSave), "imsave");
+    m->add(chaiscript::fun<bool, const std::string&, const cv::Mat_<float>&>(&linde::imSave), "imsave");
 
-     // imgproc
-     m->add(chaiscript::fun<cv::Mat_<glm::vec3>, const cv::Mat_<glm::vec3>&, float>(&gaussian_blur), "gaussian_blur");
-     m->add(chaiscript::fun<cv::Mat_<glm::vec3>, const cv::Mat_<glm::vec3>&, float, int>(&ComputeGaussDerivativeEnergy), "gaussian_derivative_energy");
+    // imgproc
+    m->add(chaiscript::fun<cv::Mat_<glm::vec3>, const cv::Mat_<glm::vec3>&, float>(&gaussian_blur), "gaussian_blur");
+    m->add(chaiscript::fun<cv::Mat_<glm::vec3>, const cv::Mat_<glm::vec3>&, float, int>(&ComputeGaussDerivativeEnergy), "gaussian_derivative_energy");
 
-     // image show
-     m->add(chaiscript::fun<void, const std::string&, const cv::Mat_<glm::vec3>&, int>(&imShow), "imshow");
-     m->add(chaiscript::fun<void, const std::string&, const cv::Mat_<float>&, int>(&imShow), "imshow");
+    // image show
+    m->add(chaiscript::fun<void, const std::string&, const cv::Mat_<glm::vec3>&, int>(&imShow), "imshow");
+    m->add(chaiscript::fun<void, const std::string&, const cv::Mat_<float>&, int>(&imShow), "imshow");
 
-     chai.add(m);
+    chai.add(m);
 }
 
 void addLindeLibFunctions(chaiscript::ChaiScript &chai)
 {
-    addVectorTypes(chai);
     addImageTypes(chai);
     addImageIO(chai);
 }
